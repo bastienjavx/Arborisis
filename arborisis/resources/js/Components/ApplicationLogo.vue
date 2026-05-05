f267c509ae4aa3e5e0d9d95c78415216518a6dc4
Bastien Javaux <0xmagicduck@proton.me>
2026-05-05 18:49:47 +0200

    fix: remove w-full h-full from ApplicationLogo to prevent oversized SVG

--- arborisis/resources/js/Components/ApplicationLogo.vue
+++ arborisis/resources/js/Components/ApplicationLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full h-full">
+    <svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
             d="M16 2C16 2 8 8 8 16C8 20.4183 11.5817 24 16 24C20.4183 24 24 20.4183 24 16C24 8 16 2 16 2Z"
             stroke="currentColor"
